60dfe85ce8331adf180678535fbe652c07340bcd
TZJ <tzjxhppkdc@126.com>
2024-04-25 15:26:02 +0800

    flickr30k，对比测试3:不同queue长度下的不同batch size

--- src/subsrc/modules/__init__.py
+++ src/subsrc/modules/__init__.py
@@ -1,15 +1,19 @@
 from typing import List, Union
 
 from .model_retrieval import (
+    RetrievalModule,
     RetrievalModuleWithQueue,
     RetrievalModuleWithQueue_1,
     RetrievalModuleWithDoubleQueue,
-    RetrievalModuleWithQueueShared
+    RetrievalModuleWithQueueShared,
+    RetrievalModuleWithQueueContrast1,
+    RetrievalModuleWithQueueContrast2,
+    RetrievalModuleWithQueueContrast3
 )
 
 _models = {
     # "baseline": RetrievalModule,
-    # "aformer":RetrievalModule,
+    "aformer4_swiglu":RetrievalModule,
     "aformer_queue":RetrievalModuleWithQueue,
     "aformer_swiglu_queue":RetrievalModuleWithQueue,
     "aformer_swiglu_double_queue_new":RetrievalModuleWithDoubleQueue,
@@ -17,6 +21,9 @@ _models = {
     "aformer3_swiglu_queue_new":RetrievalModuleWithQueue_1,
     "aformer4_swiglu_queue_new":RetrievalModuleWithQueue_1,
     "aformer4_swiglu_queue_shared":RetrievalModuleWithQueueShared,
+    "aformer4_swiglu_queue_contrast1":RetrievalModuleWithQueueContrast1,
+    "aformer4_swiglu_queue_contrast2":RetrievalModuleWithQueueContrast2,
+    "aformer4_swiglu_queue_contrast3":RetrievalModuleWithQueueContrast3,
 }
 
 def build_model(config):
