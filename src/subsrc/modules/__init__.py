8a6f1928954b4babffbd300aee8c2fc0e06c9f39
TZJ <tzjxhppkdc@126.com>
2024-04-25 11:10:39 +0800

    flickr30k，对比测试3-1:没有队列下的不同batch size

--- src/subsrc/modules/__init__.py
+++ src/subsrc/modules/__init__.py
@@ -1,15 +1,17 @@
 from typing import List, Union
 
 from .model_retrieval import (
+    RetrievalModule,
     RetrievalModuleWithQueue,
     RetrievalModuleWithQueue_1,
     RetrievalModuleWithDoubleQueue,
-    RetrievalModuleWithQueueShared
+    RetrievalModuleWithQueueShared,
+    RetrievalModuleWithQueueContrast1
 )
 
 _models = {
     # "baseline": RetrievalModule,
-    # "aformer":RetrievalModule,
+    "aformer4_swiglu":RetrievalModule,
     "aformer_queue":RetrievalModuleWithQueue,
     "aformer_swiglu_queue":RetrievalModuleWithQueue,
     "aformer_swiglu_double_queue_new":RetrievalModuleWithDoubleQueue,
@@ -17,6 +19,7 @@ _models = {
     "aformer3_swiglu_queue_new":RetrievalModuleWithQueue_1,
     "aformer4_swiglu_queue_new":RetrievalModuleWithQueue_1,
     "aformer4_swiglu_queue_shared":RetrievalModuleWithQueueShared,
+    "aformer4_swiglu_queue_contrast1":RetrievalModuleWithQueueContrast1,
 }
 
 def build_model(config):
